d3ed6449629351b79cd696fab0d6ca2caa56e83c
TulkinRB <tulkinrb@gmail.com>
2023-06-25 21:16:55 +0300

    Add check for insert return value

--- src/binomialheaptester/steps/InsertStep.java
+++ src/binomialheaptester/steps/InsertStep.java
@@ -1,5 +1,7 @@
 package binomialheaptester.steps;
 import binomialheaptester.State;
+import binomialheaptester.Utils;
+import binomialheaptester.yourcode.BinomialHeap;
 
 
 public class InsertStep implements Step {
@@ -16,7 +18,24 @@ public class InsertStep implements Step {
     @Override
     public String perform(State state) {
         State.SingleHeapState heapState = state.heapStates.get(this.heapIndex);
-        heapState.heap.insert(this.key, this.info);
+        BinomialHeap.HeapItem insertedItem = heapState.heap.insert(this.key, this.info);
+        if (insertedItem == null) {
+            return "insert() returned null (instead of the inserted item)";
+        }
+        if (!insertedItem.info.equals(this.info) || insertedItem.key != this.key) {
+            return String.format(
+                    "insert() returned an incorrect item (not the inserted item). Expected (%d, %s), got (%d, %s)",
+                    this.key, this.info, insertedItem.key, insertedItem.info
+            );
+        }
+        BinomialHeap.HeapNode expectedInsertedNode = Utils.findByInfo(heapState.heap, info);
+        if (expectedInsertedNode == null) {
+            return String.format("Inserted item (%d, %s) not found in the heap", this.key, this.info);
+        }
+        if (expectedInsertedNode.item != insertedItem) {
+            return "insert() returned an incorrect item (not the inserted item). It has the correct key " +
+                    "and info, but is a different object from the one found in the heap";
+        }
         heapState.items.put(info, this.key);
         return null;
     }
